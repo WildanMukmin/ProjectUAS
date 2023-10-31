3547ccf767aaacdcf68944345addadd2cb697fae
WildanMukmin <wildanmukmin26@gmail.com>
2023-10-31 21:34:57 +0700

    membuat algoritma game dengan array, dan membuat contoh map dengan create text

--- Projek1file/main.cpp
+++ Projek1file/main.cpp
@@ -2,20 +2,90 @@
 
 int main (){
 
-	//halaman awal
-	halamanAwal();
+	// //halaman awal
+	// halamanAwal();
 
-	//verifikasi data user
-	opsiLogin();
+	// //verifikasi data user
+	// opsiLogin();
 
-	// //halaman awal game
-	// homeGame();	
-
-
-	linexy(0,0);
-	// movePlayer();
+	// // //halaman awal game
+	// // homeGame();	
 
 
+	// linexy(0,0);
+	// // movePlayer();
+
+
+
+	// char player;
+	// int xx, yy;
+	// xx = 1;
+	// yy = 0;
+	
+
+	// while(true){
+	// 	linexy(0,0);
+	// 	char map [5][5] = {
+	// 	'#','#','#','#','#',
+	// 	' ',' ',' ',' ','#',
+	// 	'#','#','#',' ','#',
+	// 	' ',' ','#',' ','#',
+	// 	' ',' ','#',' ','#',
+	// };
+	// map [xx][yy] = 'A';
+
+	// for(int i = 0 ; i < 5; i++){
+	// 	for(int j = 0; j<5 ; j++){
+	// 		cout << map[i][j]<< ' ';
+	// 	}
+	// 	cout<<endl;
+	// }
+	// player = getch();
+
+	// if(player == 'w' || player == 'W'){
+	// 	if(map[xx - 1][yy] == ' '){
+	// 		map[xx - 1][yy] = 'A';
+	// 		map[xx][yy] = ' ';
+	// 		xx--;
+	// 	}else if (map[xx - 1][yy] == '#'){
+	// 		cout << "kalah";
+	// 		break;
+	// 	}
+	// }
+	// else if(player == 's' || player == 'S'){
+	// 	if(map[xx + 1][yy] == ' '){
+	// 		map[xx + 1][yy] = 'A';
+	// 		map[xx][yy] = ' ';
+	// 		xx++;
+	// 	}else if (map[xx + 1][yy] == '#'){
+	// 		cout << "kalah";
+	// 		break;
+	// 	}
+	// }
+	// else if(player == 'd' || player == 'D'){
+	// 	if(map[xx][yy + 1] == ' '){
+	// 		map[xx][yy + 1] = 'A';
+	// 		map[xx][yy] = ' ';
+	// 		yy++;
+	// 	}else if (map[xx][yy + 1] == '#'){
+	// 		cout << "kalah";
+	// 		break;
+	// 	}
+	// }
+	// else if(player == 'a' || player == 'A'){
+	// 	if(map[xx][yy - 1] == ' '){
+	// 		map[xx][yy - 1] = 'A';
+	// 		map[xx][yy] = ' ';
+	// 		yy--;
+	// 	}else if (map[xx][yy - 1] == '#'){
+	// 		cout << "kalah";
+	// 		break;
+	// 	}
+	// }
+
+	// }
+
+	contohGame();
 
 	return 0;
 
