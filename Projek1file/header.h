dd5b9959f05df220be3816c2a3b72462cf4e4aa0
WildanMukmin <wildanmukmin26@gmail.com>
2023-11-04 16:42:00 +0700

    meanmbahkan kode warna di file header dan di oret oretan

--- Projek1file/header.h
+++ Projek1file/header.h
@@ -10,6 +10,47 @@
 
 using namespace std;
 
+//-------------------------- Tempat Warna --------------------------//
+#define RESET			"\033[0m"
+
+#define BLACK 			"\033[30m"
+#define RED 			"\033[31m"
+#define GREEN 			"\033[32m"
+#define YELLOW 			"\033[33m"
+#define BLUE 			"\033[34m"
+#define MAGENTA 		"\033[35m"
+#define CYAN 			"\033[36m"
+#define WHITE 			"\033[37m"
+
+#define GRAY_BG 		"\033[1;30m"
+#define RED_BG 			"\033[1;31m"
+#define GREEN_BG 		"\033[1;32m"
+#define YELLOW_BG 		"\033[1;33m"
+#define BLUE_BG 		"\033[1;34m"
+#define MAGENTA_BG 		"\033[1;35m"
+#define CYAN_BG 		"\033[1;36m"
+#define WHITE_BG 		"\033[1;37m"
+
+#define BLACK_LIGHT	 	"\033[40m"
+#define RED_LIGHT	 	"\033[41m"
+#define GREEN_LIGHT	 	"\033[42m"
+#define YELLOW_LIGHT	"\033[43m"
+#define BLUE_LIGHT	 	"\033[44m"
+#define MAGENTA_LIGHT	"\033[45m"
+#define CYAN_LIGHT	 	"\033[46m"
+#define WHITE_LIGHT	 	"\033[47m"
+
+#define BLACK_BG 		"\033[1;40m"
+#define RED_BG 			"\033[1;41m"
+#define GREEN_BG 		"\033[1;42m"
+#define YELLOW_BG 		"\033[1;43m"
+#define BLUE_BG 		"\033[1;44m"
+#define MAGENTA_BG 		"\033[1;45m"
+#define CYAN_BG 		"\033[1;46m"
+#define WHITE_BG 		"\033[1;47m"
+
+//----------------------- Akhir Tempat Warna -----------------------//
+
 //delay eksekusion (int)
 void delay(int i){
 	int count = 0;
--- Projek1file/oretan.h
+++ Projek1file/oretan.h
@@ -108,3 +108,45 @@ o`   'oooo()  | ________   _   _)
     |  /                                                                                         /.
     \_/_________________________________________________________________________________________/.
 */
+
+/*
+  Warna Teks:
+    "\033[30m": Hitam (Black)
+    "\033[31m": Merah (Red)
+    "\033[32m": Hijau (Green)
+    "\033[33m": Kuning (Yellow)
+    "\033[34m": Biru (Blue)
+    "\033[35m": Magenta (Purple)
+    "\033[36m": Cyan (Cyan)
+    "\033[37m": Putih (White)
+
+  Warna Teks Terang (Bright Text):
+    "\033[1;30m": Abu-Abu Terang (Bright Black)
+    "\033[1;31m": Merah Terang (Bright Red)
+    "\033[1;32m": Hijau Terang (Bright Green)
+    "\033[1;33m": Kuning Terang (Bright Yellow)
+    "\033[1;34m": Biru Terang (Bright Blue)
+    "\033[1;35m": Magenta Terang (Bright Purple)
+    "\033[1;36m": Cyan Terang (Bright Cyan)
+    "\033[1;37m": Putih Terang (Bright White)
+
+  Latar Belakang Teks:
+    "\033[40m": Latar Belakang Hitam (Black Background)
+    "\033[41m": Latar Belakang Merah (Red Background)
+    "\033[42m": Latar Belakang Hijau (Green Background)
+    "\033[43m": Latar Belakang Kuning (Yellow Background)
+    "\033[44m": Latar Belakang Biru (Blue Background)
+    "\033[45m": Latar Belakang Magenta (Purple Background)
+    "\033[46m": Latar Belakang Cyan (Cyan Background)
+    "\033[47m": Latar Belakang Putih (White Background)
+
+  Latar Belakang Teks Terang (Bright Background):
+    "\033[1;40m": Latar Belakang Hitam Terang (Bright Black Background)
+    "\033[1;41m": Latar Belakang Merah Terang (Bright Red Background)
+    "\033[1;42m": Latar Belakang Hijau Terang (Bright Green Background)
+    "\033[1;43m": Latar Belakang Kuning Terang (Bright Yellow Background)
+    "\033[1;44m": Latar Belakang Biru Terang (Bright Blue Background)
+    "\033[1;45m": Latar Belakang Magenta Terang (Bright Purple Background)
+    "\033[1;46m": Latar Belakang Cyan Terang (Bright Cyan Background)
+    "\033[1;47m": Latar Belakang Putih Terang (Bright White Background)
+*/
