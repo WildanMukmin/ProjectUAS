2b66372327234d9e774d6c90c2ed48d4433f0c20
WildanMukmin <wildanmukmin26@gmail.com>
2023-11-26 22:42:16 +0700

    rapih rapih dari empeb

--- Projek1file/intro.cpp
+++ Projek1file/intro.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 
-void playMusic(const char* filePath) {
-    PlaySound(TEXT("music/gamestart.wav"), NULL, SND_FILENAME | SND_ASYNC);
+void playMusic(const char *filePath)
+{
+	PlaySound(TEXT(filePath), NULL, SND_FILENAME | SND_ASYNC);
 }
 using namespace std;
 
@@ -19,40 +20,51 @@ void textIntro()
 	start_color();
 	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, " _____                                                                                                         _____  ");y++;
+	mvprintw(y, x, " _____                                                                                                         _____  ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, "( ___ )-------------------------------------------------------------------------------------------------------( ___ ) ");y++;
+	mvprintw(y, x, "( ___ )-------------------------------------------------------------------------------------------------------( ___ ) ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, " |   |                                                                                                         |   |  ");y++;
+	mvprintw(y, x, " |   |                                                                                                         |   |  ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, " |   |            _____ ____  _____    _    ____  _   _ ____  _____      _   _ _   _ _   _ _____               |   |  ");y++;
+	mvprintw(y, x, " |   |            _____ ____  _____    _    ____  _   _ ____  _____      _   _ _   _ _   _ _____               |   |  ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, " |   |           |_   _|  _ \\| ____|  / \\  / ___|| | | |  _ \\| ____|    | | | | | | | \\ | |_   _|              |   |  ");y++;
+	mvprintw(y, x, " |   |           |_   _|  _ \\| ____|  / \\  / ___|| | | |  _ \\| ____|    | | | | | | | \\ | |_   _|              |   |  ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, " |   |             | | | |_) |  _|   / _ \\ \\___ \\| | | | |_) |  _|      | |_| | | | |  \\| | | |                |   |  ");y++;
+	mvprintw(y, x, " |   |             | | | |_) |  _|   / _ \\ \\___ \\| | | | |_) |  _|      | |_| | | | |  \\| | | |                |   |  ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, " |   |             | | |  _ <| |___ / ___ \\ ___) | |_| |  _ <| |___     |  _  | |_| | |\\  | | |                |   |  ");y++;
-	mvprintw(y,x, " |   |             |_| |_| \\_\\_____/_/   \\_\\____/ \\___/|_| \\_\\_____|    |_| |_|\\___/|_| \\_| |_|                |   |  ");y++;
+	mvprintw(y, x, " |   |             | | |  _ <| |___ / ___ \\ ___) | |_| |  _ <| |___     |  _  | |_| | |\\  | | |                |   |  ");
+	y++;
+	mvprintw(y, x, " |   |             |_| |_| \\_\\_____/_/   \\_\\____/ \\___/|_| \\_\\_____|    |_| |_|\\___/|_| \\_| |_|                |   |  ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, " |   |                                                                                                         |   |  ");y++;
-	mvprintw(y,x, " |___|                                                                                                         |___|  ");y++;
+	mvprintw(y, x, " |   |                                                                                                         |   |  ");
+	y++;
+	mvprintw(y, x, " |___|                                                                                                         |___|  ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(1));
-	mvprintw(y,x, "(_____)-------------------------------------------------------------------------------------------------------(_____) ");y++;
+	mvprintw(y, x, "(_____)-------------------------------------------------------------------------------------------------------(_____) ");
+	y++;
 	attroff(COLOR_PAIR(1));
 	Sleep(50);
 	getch();
 }
 
-
-int main(){
+int main()
+{
 	initscr();
 	playMusic("music/gamestart.wav");
 	textIntro();
@@ -64,10 +76,11 @@ int main(){
 	init_pair(1, COLOR_CYAN, COLOR_BLACK);
 	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
 	attron(COLOR_PAIR(2));
-	mvprintw(25,x, "LET'S START !");
+	mvprintw(25, x, "LET'S START !");
 	Sleep(50);
 	attroff(COLOR_PAIR(1));
-	for(int i = 74; i <= 100; i++){
+	for (int i = 74; i <= 100; i++)
+	{
 		attron(COLOR_PAIR(2));
 		mvprintw(25, i, ">>>");
 		refresh();
@@ -79,4 +92,3 @@ int main(){
 	system("game");
 	system("cls");
 }
-
